4d5e47feeb6429af422f5baea6df9770fcad31c2
Md Aminul Islam <mr.aminul118@gmail.com>
2025-05-02 03:31:27 +0600

    Founder name change

--- src/components/About/Founder.tsx
+++ src/components/About/Founder.tsx
@@ -4,7 +4,7 @@ const Founder = () => {
   return (
     <section className="text-center space-y-4 py-16 2xl:py-24">
       <User className="mx-auto h-10 w-10" />
-      <h2 className="text-2xl font-semibold">Mr. Saikot</h2>
+      <h2 className="text-2xl font-semibold">Mr. MD Saifur Rahman Saikot</h2>
       <p className="text-muted-foreground max-w-xl mx-auto">
         Founder & Chief Executive Officer of Digital Twin Energy LLC. With a
         vision to integrate AI into every layer of energy production, Mr. Saikot
