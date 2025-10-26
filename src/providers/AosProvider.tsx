9e88fc89a576e277ad5ca45180be404d68aa8f78
MD AMINUL ISLAM <mr.aminul118@gmail.com>
2025-10-26 12:49:53 +0600

    Next js version upgrade to 16

--- src/providers/AosProvider.tsx
+++ src/providers/AosProvider.tsx
@@ -1,14 +1,22 @@
-'use client';
+"use client";
 
-import { IChildren } from '@/types';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import { IChildren } from "@/types";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect, useState } from "react";
 
 const AosProvider = ({ children }: IChildren) => {
+  const [isClient, setIsClient] = useState(false);
+
   useEffect(() => {
-    AOS.init({ duration: 1000, once: true });
+    setIsClient(true);
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
   }, []);
+
+  if (!isClient) return null;
 
   return <div className="overflow-x-hidden">{children}</div>;
 };
